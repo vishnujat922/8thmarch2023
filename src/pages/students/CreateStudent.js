755eb252478533e4139d3f03932db5a1587206b6
vishnujat922 <vishnujat922@gmail.com>
2023-03-09 20:28:52 +0530

    v1

--- src/pages/students/CreateStudent.js
+++ src/pages/students/CreateStudent.js
@@ -1,30 +1,77 @@
 //1. Import Area
-import React from 'react'
-import { Form, Button,Table } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Form, Button, Table } from 'react-bootstrap'
 //2. Definition Area
 export default function CreateStudent() {
+  //2.1 Hooks Area
+  //let teacher='';
+  const [teacher, setTeacher] = useState([]);
+  //useSomething() will be A Hook Function
+  //useEffect(cbfn,dependency Array)
+  useEffect(() => {
+    // I want to call The get all Teacher api
+    fetch('http://localhost:1337/api/teachers', {
+      method: "GET"
+    })
+      .then(res=>res.json())
+      .then((data)=>{
+        console.log(data.data);
+        setTeacher(data.data)
+        //set the hook variable
+      })
+      .catch(() => {
+      });
+  }, []);
+  //2.2 Definition Area
+  let createStudent = () => {
+    //alert('Hello Student');
+    let payload ={
+      "data": {
+        "name": document.getElementById('student_name').value,
+        "teachers":[parseInt(document.getElementById('teacher').value)]
+      }
+    }
+    // our Payload is Ready to send to server
+    console.log(payload);
+    fetch(`http://localhost:1337/api/students`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    })
+      .then(res => res.json())
+      .then(data => {
+        alert("Student Data Inserted Successfully");
+        console.log(data);
+      })
+      .catch();
+  }
+
+  //2.3 Return Statement
   return (
     <>
       <div className='container'>
         <h1 className='text-center mt-5'>Create Student</h1>
         <Form>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Student Name</Form.Label>
-            <Form.Select aria-label="Default select example">
-              <option> Select Teacher</option>
-              <option value="1">TeacherA</option>
-              <option value="2">TeacherB</option>
-              <option value="3">TeacherC</option>
-              <option value="4">TeacherD</option>
+            <Form.Label>Selection Teachers</Form.Label>
+            <Form.Select id="teacher" aria-label="Default select example">
+              {
+                teacher.map((cv, idx, arr) => {
+                  console.log(cv);
+                  return <option key={idx} value={cv.id}>{cv.attributes.name}</option>
+                })
+              }
             </Form.Select>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Student Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter name" />
+            <Form.Control id="student_name" type="text" placeholder="Enter name" />
             <Form.Text className="text-muted">
             </Form.Text>
           </Form.Group>
-          <Button variant="primary" type="submit">
+          <Button variant="primary" type="button" onClick={() => { createStudent() }}>
             Submit
           </Button>
         </Form>
@@ -33,7 +80,7 @@ export default function CreateStudent() {
         <br />
         <Table striped bordered hover>
           <thead>
-            <tr>
+            <tr className='text-center'>
               <th>Id</th>
               <th>Student Name</th>
               <th>Action</th>
@@ -42,7 +89,9 @@ export default function CreateStudent() {
           <tbody>
             <tr>
               <td>1</td>
-              <td>Mark</td>
+              <td>Vishnu Jaat
+                
+              </td>
               <td>
                 <Button className='btn btn-primary btn-sm me-2 '>View</Button>
                 <Button className='btn btn-success btn-sm me-2'>Edit</Button>
